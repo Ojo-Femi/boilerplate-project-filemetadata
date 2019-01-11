51fc3a3ce06304a96b5ac950f9f1e4c0d15f24bd
Ojo-Femi Oma-Victor <ojokotor@gmail.com>
2019-01-11 12:51:45 +0100

    Update server,js with multer
    Used multer to intercept the file upload and have a file object. Pretty awesome!

--- server.js
+++ server.js
@@ -3,9 +3,11 @@
 var express = require('express');
 var cors = require('cors');
 
-// require and use "multer"...
 
+// require and use "multer"...
+var multer = require('multer');
 var app = express();
+var upload = multer( {dest: './assets/'} )
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -17,6 +19,19 @@ app.get('/', function (req, res) {
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
+
+app.post("/api/fileanalyse", upload.single('upfile'), (req,res)=>{
+  var filename = '';
+  if (req.file){
+   console.log(req.file);
+    filename = req.file.originalname;
+    //uploadStatus = 'File Uploaded successfully';
+  } else {
+    filename = "File Not Uploaded"
+    //uploadStatus = 'File upload failed';
+  }
+  res.json({name: filename, type:req.file.mimetype, size: req.file.size});
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
